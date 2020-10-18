70cdf941748b4aff538636e9e92c3269c095f9cd
wagner lemos <wagner.lemos94@hotmail.com>
2020-10-18 19:03:17 -0300

    Frist controller and Razor pages tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,8 +13,8 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web MVC app from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
             return View();
         }
 
